5628efc2a3e5eb15fe246ac32a6ddc2a23087b25
Abeyruwan <kmld333aberuwan@gmail.com>
2025-08-15 14:33:47 +0530

    create footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <p>Welcome To Anju Mobile</p>
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <h1>Welcome To Anju Mobile</h1>
+      </Container>
+    </main>
+    <Footer />
+    </>
+    
   )
 }
 
-export default App
+export default App;
