4e12bf9aeae771bf1b15c2e618b805d8d51d00c3
Abeyruwan <kmld333aberuwan@gmail.com>
2025-09-25 20:45:00 +0530

    set the backend authentication

--- backend/data/users.js
+++ backend/data/users.js
@@ -3,21 +3,21 @@ import bcrypt from "bcryptjs";
 const users = [
   {
     id: 1,
-    username: "Admin User",
+    name: "Admin User",
     email: "admin@gmail.com",
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true, 
   },
   {
     id: 2,
-    username: "jane_doe",
+    name: "jane_doe",
     email: "jane@gmail.com",
     password: bcrypt.hashSync("123456", 10),
     isAdmin: false,
   },
   {
     id: 3,
-    username: "john_doe",
+    name: "john_doe",
     email: "john@gmail.com",
     password: bcrypt.hashSync("123456", 10),
     isAdmin: false,
